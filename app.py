5cddf43f0b0e1bb9bad5d2927ecc22454e87f97d
Shubham Garg <74714209+xxshubhamxx@users.noreply.github.com>
2023-06-02 10:39:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,27 @@
 from flask import Flask, render_template, request, redirect, url_for
 from PIL import Image
 import io
+import tensorflow as tf
 
 app = Flask(__name__)
 
+model_path = "m1.h5"
+model = tf.keras.models.load_model(model_path)
+
+def preprocess_image(image):
+    # Add your preprocessing steps, like resizing, scaling, etc.
+    # This example assumes the model expects a 224x224 image
+    img = image.resize((256, 256))
+    img_array = tf.keras.preprocessing.image.img_to_array(img)
+    img_array = img_array / 255.0
+    img_array = tf.expand_dims(img_array, 0)
+    return img_array
+
 def predict(image):
-    # TODO: Load your model and predict the class
-    return "Normal"
+    processed_image = preprocess_image(image)
+    prediction = model.predict(processed_image)
+    predicted_class = "Normal" if prediction[0][0] > 0.5 else "Abnormal"
+    return predicted_class
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
